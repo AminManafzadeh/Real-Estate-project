12ef5b06ec3e045bbc0a4d4d4e2d50b630688042
AminManafzadeh <amin.mah1375@gmail.com>
2025-08-29 10:48:02 -0700

    changes model folder

--- src/components/layout/DashboardSidebar.jsx
+++ src/components/layout/DashboardSidebar.jsx
@@ -1,19 +1,16 @@
-import { authOption } from "@/app/api/auth/[...nextauth]/route";
 import LogoutButton from "@/module/LogoutButton";
-import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 
-async function DashboardSidebar({ children }) {
-  const session = await getServerSession(authOption);
-
+async function DashboardSidebar({ children, role, email }) {
   return (
     <div className="flex justify-between mt-20">
       <div className="flex flex-col items-center h-fit py-[30px] px-[15px] rounded-[10px] shadow-custom ml-10 w-[220px]">
         <CgProfile className="text-5xl text-mainBlue" />
-        <p className="text-gray text-lg font-normal mt-5">
-          {session?.user?.email}
+        <p className="text-gray text-lg font-normal">
+          {role === "ADMIN" ? "ادمین" : null}
         </p>
+        <p className="text-gray text-lg font-normal mt-5">{email}</p>
         <span className="bg-silver w-full h-[1px] mb-[30px]"></span>
         <Link className="my-[5px] font-normal w-full" href="/dashboard">
           حساب کاربری
@@ -27,6 +24,11 @@ async function DashboardSidebar({ children }) {
         <Link className="my-[5px] font-normal w-full" href="/dashboard/add">
           ثبت آگهی
         </Link>
+        {role === "ADMIN" ? (
+          <Link className="my-[5px] font-normal w-full" href="/admin">
+            در انتظار تایید ...
+          </Link>
+        ) : null}
         <LogoutButton />
       </div>
       <div className="w-full">{children}</div>
